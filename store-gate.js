76c1442e6b9d05b0389f1701280a0d99690a1a73
Zapak69 <rigson00@gmail.com>
2026-09-05 00:26:46 +0200

    update

--- store-gate.js
+++ store-gate.js
@@ -2,7 +2,7 @@
   let hasAccess = false;
   try { hasAccess = localStorage.getItem('storeAccess') === 'true'; } catch (e) {}
 
-  const onComingSoon = /(^|\/)coming-soon\.html$/.test(location.pathname);
+  const onComingSoon = /\/coming-soon(\.html)?\/?$/.test(location.pathname);
 
   if (hasAccess && onComingSoon) {
     location.replace('index');
